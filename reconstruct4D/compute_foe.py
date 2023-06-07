1ada781b106c29ba48e79d7f2a657584ad4edcb0
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2023-06-08 08:49:28 +0900

    load image files and display it.

--- reconstruct4D/compute_foe.py
+++ reconstruct4D/compute_foe.py
@@ -1,10 +1,35 @@
 # %%
-import opticalflow
+import sys
+sys.path.append('..')
+import reconstruct4D.opticalflow as opticalflow
+import os
+import cv2
 
 def main():
+    # preparation
+    # TODO: use argparse. but currently to use jupyter, we cannot use argparse.
+    image_dir = '/mnt/data/study/mine/computer_vision/todaiura/images/480p'
+    print(image_dir)
+
+    # get image files
+    imgfiles = sorted([file for file in os.listdir(image_dir) if file.endswith('.jpg') or file.endswith('.png')])
+    print(imgfiles)
     flow = opticalflow.UnimatchFlow()
+
+    # process each image
+    for imgname in imgfiles:
+        img = cv2.imread(os.path.join(image_dir, imgname))
+
+        # debug
+        print(imgname)
+        print(img.shape)
+        cv2.imshow('img', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
     flow.compute(None, None)
 
 if __name__ == '__main__':
     main()
+
 # %%
--- reconstruct4D/opticalflow.py
+++ reconstruct4D/opticalflow.py
@@ -2,30 +2,31 @@
 import sys
 sys.path.append('../unimatch')
 import main_flow
+import unimatch
 import torch
-import unimatch.unimatch as unimatch
 
 class UnimatchFlow():
     '''
     compute optical flow using unimatch algorithm
     '''
     def __init__(self) -> None:
-        # treat args as a global variable
-        parser = main_flow.get_args_parser()
-        args = parser.parse_args()
-        print(args)
+        pass
+        # # treat args as a global variable
+        # parser = main_flow.get_args_parser()
+        # args = parser.parse_args()
+        # print(args)
 
-        # load model
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        model = unimatch.UniMatch(feature_channels=args.feature_channels,
-                            num_scales=args.num_scales,
-                            upsample_factor=args.upsample_factor,
-                            num_head=args.num_head,
-                            ffn_dim_expansion=args.ffn_dim_expansion,
-                            num_transformer_layers=args.num_transformer_layers,
-                            reg_refine=args.reg_refine,
-                            task=args.task).to(device)
-        print(model)
+        # # load model
+        # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # model = unimatch.UniMatch(feature_channels=args.feature_channels,
+        #                     num_scales=args.num_scales,
+        #                     upsample_factor=args.upsample_factor,
+        #                     num_head=args.num_head,
+        #                     ffn_dim_expansion=args.ffn_dim_expansion,
+        #                     num_transformer_layers=args.num_transformer_layers,
+        #                     reg_refine=args.reg_refine,
+        #                     task=args.task).to(device)
+        # print(model)
 
     def compute(self, img1, img2):
         '''
