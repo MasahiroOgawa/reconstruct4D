b9879efa8cda426132d32768a12fb97782f467cf
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2023-06-10 16:18:51 +0900

    draw debugging flow arrow.

--- reconstruct4D/compute_foe.py
+++ reconstruct4D/compute_foe.py
@@ -1,4 +1,3 @@
-# %%
 import os
 import cv2
 import sys
@@ -6,50 +5,48 @@ sys.path.append('..')
 import reconstruct4D.opticalflow as opticalflow
 import reconstruct4D.focus_of_expansion as focus_of_expansion
 
-# def main():
+def main():
+    # paramaters
+    # TODO: use argparse. but currently to use jupyter, we cannot use argparse.
+    image_dir = '/mnt/data/study/mine/computer_vision/todaiura/images/480p'
 
-# paramaters
-# TODO: use argparse. but currently to use jupyter, we cannot use argparse.
-image_dir = '/mnt/data/study/mine/computer_vision/todaiura/images/480p'
+    # preparation
+    imgfiles = sorted([file for file in os.listdir(image_dir) if file.endswith('.jpg') or file.endswith('.png')])
+    print(f"reading input image files: {imgfiles}")
+    unimatch = opticalflow.UnimatchFlow()
+    prev_img = None
+    foe = focus_of_expansion.FoE(f=1000)
 
-# preparation
-imgfiles = sorted([file for file in os.listdir(image_dir) if file.endswith('.jpg') or file.endswith('.png')])
-print(f"reading input image files: {imgfiles}")
-unimatch = opticalflow.UnimatchFlow()
-prev_img = None
-foe = focus_of_expansion.FoE()
+    # process each image
+    for imgname in imgfiles:
+        img = cv2.imread(os.path.join(image_dir, imgname))
 
-# %%imgname
-# process each image
-for imgname in imgfiles:
-    img = cv2.imread(os.path.join(image_dir, imgname))
+        if prev_img is None:
+            prev_img = img
+            continue
 
-    if prev_img is None:
+        # debug
+        print(f"{imgname} : {img.shape}")
+
+        # currently just read flow from corresponding image file name.
+        unimatch.compute(imgname)
+        
+        # display the esult
+        result_img = cv2.vconcat([img, unimatch.flow_img])
+        cv2.imshow('result', result_img)
+        key = cv2.waitKey(0)
+        if key == ord('q'):
+            break
+
+        # compute focus of expansion
+        foe.compute(unimatch.flow, unimatch.flow_img)
+
+        # prepare for the next frame
         prev_img = img
-        continue
 
-    # debug
-    print(f"{imgname} : {img.shape}")
+    cv2.destroyAllWindows()
 
-    # currently just read flow from corresponding image file name.
-    unimatch.compute(imgname)
-    
-    # display the esult
-    result_img = cv2.vconcat([img, unimatch.flow_img])
-    cv2.imshow('result', result_img)
-    key = cv2.waitKey(0)
-    if key == ord('q'):
-        break
 
-    foe.compute(unimatch.flow)
-
-    # prepare for the next frame
-    prev_img = img
-
-cv2.destroyAllWindows()
-
-# %%
-
-# %%
 if __name__ == '__main__':
     main()
+
--- reconstruct4D/focus_of_expansion.py
+++ reconstruct4D/focus_of_expansion.py
@@ -1,9 +1,29 @@
-class FoE():
-    def __init__(self) -> None:
-        pass
+import numpy as np
+import cv2
 
-    def compute(self, flow):
+class FoE():
+    def __init__(self, f) -> None:
+        self.f = f
+
+    def compute(self, flow, flow_img):
         '''
         compute focus of expansion from optical flow.
         '''
-        pass
+        
+        # compute arrow for every 10 pixels
+        for i in range(0, flow.shape[0], 10):
+            for j in range(0, flow.shape[1], 10):
+                 x = [j, i, 1]
+                 
+                 # flow
+                 u = flow[i, j, 0]
+                 v = flow[i, j, 1]
+                 x_prev = [j - u, i - v, 1]
+
+                 # debug. draw arrow
+                 cv2.arrowedLine(flow_img, (j, i), (int(j - u), int(i - v)), (0, 0, 255), 1)
+                 cv2.imshow('flow arrow', flow_img)
+
+                 # no rotation correction version
+                 l1 = np.cross(x, x_prev)
+
