d847ee2dcaadd77f287c77f0928a8bc919235c8d
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2023-06-11 11:59:30 +0900

    debug display for each lines for FoE.

--- reconstruct4D/focus_of_expansion.py
+++ reconstruct4D/focus_of_expansion.py
@@ -10,19 +10,31 @@ class FoE():
         compute focus of expansion from optical flow.
         '''
         self.flow = flow
-        self.flow_img = flow_img 
+        self.foe_img = flow_img 
+        self.debug_img = flow_img
 
         # randomly select 2 points from flow
         for _ in range(100):
             i, j = np.random.randint(0, flow.shape[0]), np.random.randint(0, flow.shape[1])
             l1 = self.comp_flowline(i, j)
-            i, j = np.random.randint(0, flow.shape[0]), np.random.randint(0, flow.shape[1])
-            l2 = self.comp_flowline(i, j)
-            foe = np.cross(l1, l2)
-            self.draw_homogeneous_point(foe)
+            for _ in range(10):
+                self.debug_img = self.foe_img.copy()
+
+                i, j = np.random.randint(0, flow.shape[0]), np.random.randint(0, flow.shape[1])
+                l2 = self.comp_flowline(i, j)
+                foe = np.cross(l1, l2)
+
+                # debug. draw line
+                self.draw_line(l1)
+                self.draw_line(l2)
+                self.draw_homogeneous_point(foe, self.debug_img)
+                cv2.imshow('FoE', self.debug_img)
+                key = cv2.waitKey(0)
+                if key == ord('q'):
+                    exit()
 
         # draw flow image
-        cv2.imshow('flow arrow', self.flow_img)
+        cv2.imshow('foe', self.foe_img)
         key = cv2.waitKey(0)
         if key == ord('q'):
             exit()
@@ -36,17 +48,24 @@ class FoE():
         x_prev = [j - u, i - v, 1]
 
         # debug. draw arrow
-        cv2.arrowedLine(self.flow_img, list(map(int, x_prev[0:2])), x[0:2], (0, 0, 255), 1)
+        cv2.arrowedLine(self.debug_img, list(map(int, x_prev[0:2])), x[0:2], (0, 0, 255), 3)
 
         # no rotation correction version
         line = np.cross(x, x_prev)
         return line
 
 
-    def draw_homogeneous_point(self, hom_pt):
+    def draw_homogeneous_point(self, hom_pt, out_img):
         if hom_pt[2] == 0:
             return
         pt = (int(hom_pt[0] / hom_pt[2]), int(hom_pt[1] / hom_pt[2]))
-        # draw cross on the point
-        cv2.line(self.flow_img, (pt[0] - 10, pt[1]), (pt[0] + 10, pt[1]), (0, 0, 255), 10)
-        cv2.line(self.flow_img, (pt[0], pt[1] - 10), (pt[0], pt[1] + 10), (0, 0, 255), 10)
+        # draw cross at the point
+        cv2.line(out_img, (pt[0] - 10, pt[1]), (pt[0] + 10, pt[1]), (0, 0, 255), 10)
+        cv2.line(out_img, (pt[0], pt[1] - 10), (pt[0], pt[1] + 10), (0, 0, 255), 10)
+
+    def draw_line(self, line):
+        if line[0] == 0 and line[1] == 0:
+            return
+        pt1 = (0, int(-line[2] / line[1]))
+        pt2 = (self.foe_img.shape[1], int(-(line[2] + line[0] * self.foe_img.shape[1]) / line[1]))
+        cv2.line(self.debug_img, pt1, pt2, (0, 255, 0), 1)
