c19e09150f97f217f4e7a9beb8445d692b4bd7a9
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2023-06-11 10:39:30 +0900

    create comp_flowline().

--- reconstruct4D/focus_of_expansion.py
+++ reconstruct4D/focus_of_expansion.py
@@ -9,21 +9,28 @@ class FoE():
         '''
         compute focus of expansion from optical flow.
         '''
-        
+        self.flow = flow
+        self.flow_img = flow_img 
+
         # compute arrow for every 10 pixels
         for i in range(0, flow.shape[0], 10):
             for j in range(0, flow.shape[1], 10):
-                 x = [j, i, 1]
-                 
-                 # flow
-                 u = flow[i, j, 0]
-                 v = flow[i, j, 1]
-                 x_prev = [j - u, i - v, 1]
+                self.comp_flowline(i, j)
 
-                 # debug. draw arrow
-                 cv2.arrowedLine(flow_img, list(map(int, x_prev[0:2])), x[0:2], (0, 0, 255), 1)
-                 cv2.imshow('flow arrow', flow_img)
 
-                 # no rotation correction version
-                 l1 = np.cross(x, x_prev)
 
+    def comp_flowline(self, i: int, j: int):
+        x = [j, i, 1]
+            
+        # flow
+        u = self.flow[i, j, 0]
+        v = self.flow[i, j, 1]
+        x_prev = [j - u, i - v, 1]
+
+        # debug. draw arrow
+        cv2.arrowedLine(self.flow_img, list(map(int, x_prev[0:2])), x[0:2], (0, 255, 0), 1)
+        cv2.imshow('flow arrow', self.flow_img)
+
+        # no rotation correction version
+        line = np.cross(x, x_prev)
+        return line
