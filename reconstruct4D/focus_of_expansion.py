a23afdbf662a4b4b9ca640bd1d1e7c4feadf648f
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2025-05-11 18:47:47 +0900

    debu by adding foe_candi_uvcoordi

--- reconstruct4D/focus_of_expansion.py
+++ reconstruct4D/focus_of_expansion.py
@@ -187,6 +187,7 @@ class FoE:
                 self.foe = refined_foe
 
             if self.LOG_LEVEL > 0:
+                foe_candi_uvcoordi = foe_candi[0:2] / foe_candi[2]
                 foe_uvcoordi = self.foe[0:2] / self.foe[2]
                 # check distance from foe_candi to foe
                 print(
